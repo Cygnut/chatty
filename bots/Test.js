7bfd56ed72f705d14a98ea42372cc8c5e3ec3217
lee <lee@feeditback.co.uk>
2022-03-01 14:29:03 +0000

    fix ~test

--- bots/Test.js
+++ bots/Test.js
@@ -5,9 +5,9 @@ export default class Test extends Bot {
     #from = 'Test-a-bot';
 
     constructor() {
-        super({ 
-            name: 'test', 
-            description: "Tests the all installed bots." 
+        super({
+            name: 'test',
+            description: "Tests the all installed bots."
         });
     }
 
@@ -15,10 +15,10 @@ export default class Test extends Bot {
         // Get an array of tests for each bot.
         const botTests = getBotMetadata()
             .map(bot => { return bot.tests; })
-    
+
         // Merge them all into one array.
         const allTests = [].concat.apply([], botTests);
-    
+
         // Generate the full test suite:
         const tests = allTests;
         tests.unshift('STARTING TESTS NOW');
@@ -31,22 +31,22 @@ export default class Test extends Bot {
     }
 
     async onDirectMessage() {
-        const tests = this.generateTests(this.host.getBotMetadata);
+        const tests = this.generateTests(this.host.getBotMetadata.bind(this.host));
         console.log(tests);
-    
+
         // Copy the array into a reversed queue.
         const queue = tests.slice().reverse();
-    
+
         let timerId = null;
         timerId = setInterval(() => {
             const message = queue.pop();
-        
+
             if (message === undefined) {
                 console.log('No messages left to send - finished sending.');
                 clearInterval(timerId);
                 return;
             }
-        
+
             this.host.respond(this.#from, message);
         }, 1000);
     }
