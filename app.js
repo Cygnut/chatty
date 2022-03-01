3ef6aca8936139b7f74e687f20fb3216b20a6551
Cygnut <ligand@hotmail.co.uk>
2022-03-01 23:26:06 +0000

    hmm

--- app.js
+++ app.js
@@ -28,11 +28,12 @@ import Loader from './bot/Loader.js';
     };
     host.addBots(bots);
 
-    [
+    const channels = [
         new Remote(url, msg => host.onMessage(msg, false)),
         new Console(msg => host.onMessage(msg, true))
-    ]
-    .forEach(channel => channel.receive());
+    ];
+
+    channels.forEach(channel => channel.receive());
 })();
 
 
