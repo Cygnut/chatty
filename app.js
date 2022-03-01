3c9f95a1817d7d1d25df0d4f1394b2a6147252e8
lee <lee@feeditback.co.uk>
2022-03-01 14:31:33 +0000

    fix post call

--- app.js
+++ app.js
@@ -17,7 +17,10 @@ import Loader from './bot/Loader.js';
     const host = new Host();
     host.respond = (from, content) => {
         try {
-            fetch.post(`${url}send`, { from, content });
+            fetch(`${url}send`, {
+              method: 'POST',
+              body: JSON.stringify({ from, content })
+            });
         } catch (e) {
             console.error(e);
         }
@@ -40,7 +43,7 @@ import Loader from './bot/Loader.js';
 
     better access to host from bots that need it (enable, help, self test), rather than hackily setting bot.host, provide limited interface
 
-    all todos   
+    all todos
     change tab size
 
     get self-test working (albeit hackily)
@@ -48,44 +51,44 @@ import Loader from './bot/Loader.js';
 
     Roadmap:
         Combine.
-    
+
     ChatBot:
-        
+
         Store enabled state of each bot persistently.
-        
+
         Fix/finish TicTacToeBot & test it!
         Finish UdpBot, and test it. It currently is not sending to the Chat.
-        
+
         WIP: Support for bots which can send not only on received data, but also on a timer?
         http://pokeapi.co/api/v2/pokemon-species/pikachu/
-        
+
         Combine Chat & ChatBot?
-        
+
         Bots:
             tic-tac-toe
             client stats - requires code merge of ChatBot & Chat
             chat stats
             dump src code (i.e. file browser)
             todos bot
-        
+
     Chat:
         Backup & Restore maybe the last 100 messages.
             WIP - need to test read & write to file first! Currently disabled.
-        
+
         Integrate self testing into app. I.e. have a bot that sends all test messages into the chat.
-        
+
         Better msg ids
-    
+
     Chat & ChatBot:
         Allow them to be hosted forever silently.
-    
-        
+
+
     Client:
         Make more efficient
-        
+
     General:
-        Create package.json: 
+        Create package.json:
             https://docs.npmjs.com/getting-started/using-a-package.json
 
-        
+
 */
