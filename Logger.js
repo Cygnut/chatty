f6468a97e8d2f107116fcda4c6b68a1013b10f53
Cygnut <ligand@hotmail.co.uk>
2022-03-01 21:23:32 +0000

    autoformat exceptions

--- Logger.js
+++ Logger.js
@@ -1,11 +1,21 @@
 import { createLogger, format, transports } from 'winston';
 
-const formatter = format((info, opts) => {
+const errorFormat = format(info => {
+  if (info && info instanceof Error) {
+      info.message = info.stack;  // This includes the message.
+  }
+  return info;
+});
+
+const prefixFormat = format((info, opts) => {
   info.message = [
     new Date().toISOString(),
     info.level.padEnd(5, ' '),
     info.message || ''
-  ].filter(v => v).join(' | ');
+  ]
+    .filter(v => v)
+    .join(' | ');
+
   // We need to define this formatter as a finalizer formatter - i.e. by setting
   // info[Symbol.for('message')] = info.message as info[Symbol.for('message')] is what's actually
   // printed, info.message is what's passed by the log call. If it isn't set, you'll just print
@@ -15,7 +25,10 @@ const formatter = format((info, opts) => {
 });
 
 const logger = createLogger({
-  format: formatter(),
+  format: format.combine(
+    errorFormat(),
+    prefixFormat()
+  ),
   transports: [
     new transports.Console()
   ]
