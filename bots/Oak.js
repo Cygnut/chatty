dcffbe7850c7cf7bd2e363ff86833e9240c022c9
lee <lee@feeditback.co.uk>
2022-03-03 09:05:40 +0000

    formatting

--- bots/Oak.js
+++ bots/Oak.js
@@ -4,43 +4,37 @@ import logger from '../Logger.js';
 import Bot from '../bot/Bot.js';
 
 export default class Oak extends Bot {
-    constructor() {
-        super({
-            name: 'oak',
-            description: "Ask Professor Oak about pokemon." }
-        );
-    }
+  constructor() {
+    super({
+      name: 'oak',
+      description: "Ask Professor Oak about pokemon." }
+    );
+  }
 
-    getTests() {
-        return [
-            `${this.name} mewtwo`
-        ];
-    }
+  getTests() {
+    return [
+      `${this.name} mewtwo`
+    ];
+  }
 
-    getEnFlavourText(result) {
-        for (let i = 0; i < result.flavor_text_entries.length; i++) {
-            const f = result.flavor_text_entries[i];
-            if (f.language.name === "en") {
-                // Could filter on f.version.name - the game name - for now though, just use the first one.
-                return `${f.flavor_text} (${f.version.name})`;
-            }
-        }
+  getEnFlavourText(result) {
+    // We could filter on entry.version.name - the game name - for now though, just use the first one.
+    const entry = result.flavor_text_entries.find((entry) => entry.language.name === "en");
+    return entry ? `${entry.flavor_text} (${entry.version.name})` : '';
+  }
 
-        return '';
+  async onDirectMessage({ content, from }) {
+    try {
+      const url = `http://pokeapi.co/api/v2/pokemon-species/${content}`;
+      const response = await fetch(url);
+      const body = await response.json();
+      this.reply(
+        `${body.name} is a ${body.color.name} ${body.shape.name} ${body.generation.name} pokemon. ${this.getEnFlavourText(body)}`,
+        from
+      );
+    } catch (e) {
+      logger.error(e);
+      this.reply("Couldn't ask Professor Oak about it..", from);
     }
-
-    async onDirectMessage({ content, from }) {
-        try {
-            const url = `http://pokeapi.co/api/v2/pokemon-species/${content}`;
-            const response = await fetch(url);
-            const body = await response.json();
-            this.reply(
-                `${body.name} is a ${body.color.name} ${body.shape.name} ${body.generation.name} pokemon. ${this.getEnFlavourText(body)}`,
-                from
-            );
-        } catch (e) {
-            logger.error(e);
-            this.reply("Couldn't ask Professor Oak about it..", from);
-        }
-    }
+  }
 }
