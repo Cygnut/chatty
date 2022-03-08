4a4f8db9fc23994807474c3d4e41a13eac9035e1
lee <lee@feeditback.co.uk>
2022-03-08 16:22:06 +0000

    rename

---
+++ bots/Console.js
@@ -0,0 +1,45 @@
+import { transports } from 'winston';
+
+import Bot from '../bot/Bot.js';
+import logger from '../Logger.js';
+
+export default class Console extends Bot {
+  constructor() {
+    super({
+      name: 'console',
+      description: "Configure the console.",
+      disableable: false,
+    });
+  }
+
+  getTests() {
+    return [
+      `${this.name} off`,
+      `${this.name} on`
+    ];
+  }
+
+  getConsoleLogTransport() {
+    return logger.transports.find(transport => transport.name === 'console');
+  }
+
+  replyWithConsoleLogTransportStatus(from) {
+    this.context.reply(`Console logging is ${this.getConsoleLogTransport().silent ? 'disbled' : 'enabled'}`, from);
+  }
+
+  enableConsoleLogTransport(on) {
+    this.getConsoleLogTransport().silent = !on;
+  }
+
+  async onDirectMessage({ content, from }) {
+    content = content.toLowerCase();
+
+    if (content === 'off') {
+      this.enableConsoleLogTransport(false);
+    } else if (content === 'on') {
+      this.enableConsoleLogTransport(true);
+    }
+
+    this.replyWithConsoleLogTransportStatus(from);
+  }
+}
