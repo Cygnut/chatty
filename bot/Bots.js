b1fe0c5d9e0143dd09169ccbd1888d9674735549
lee <lee@feeditback.co.uk>
2022-03-08 11:58:15 +0000

    use Bots

--- bot/Bots.js
+++ bot/Bots.js
@@ -1,4 +1,4 @@
-import logger from "./Logger.js";
+import logger from "../Logger.js";
 
 class Channels {
   #bots = [];
@@ -8,7 +8,41 @@ class Channels {
   }
 
   setReply(reply) {
-    this.#bots.forEach(bot => bot.reply = reply);
+    this.#bots.forEach(bot => bot.reply = reply.bind(bot));
+  }
+
+  getEnabledBots() {
+    return this.#bots.filter(bot => bot.enabled);
+  }
+
+  findMentionedBot(content) {
+      return this.getEnabledBots().find(bot => content.startsWith(bot.name));
+  }
+
+  describeBots() {
+    return this.#bots.map(bot => ({
+      name: bot.name,
+      description: bot.description,
+      enabled: bot.enabled,
+      enableable: bot.enableable,
+      tests: bot.getTests(),
+    }));
+  }
+
+  enableBot(botName, on) {
+    const bot = this.#bots.find(bot => bot.name === botName);
+
+    if (!bot)
+      return null;
+
+    // If on is not passed, then flip the state - else, set to the defined state in on.
+    if (on === undefined) {
+      bot.enable(!bot.enabled);
+    } else {
+      bot.enable(on);
+    }
+
+    return bot.enabled;
   }
 }
 
--- bot/Host.js
+++ bot/Host.js
@@ -1,61 +1,50 @@
 import logger from '../Logger.js';
+import Bots from './Bots.js'
 import Bot from './Bot.js';
 
 export default class Host {
-  #bots = [];
+  #bots;
   #channels;
 
   constructor({ channels, bots }) {
     this.#channels = channels;
     this.#channels.setOnNewMessage(msg => this.onMessage(msg));
 
-    this.#bots = bots.map(bot => {
+    this.#bots = new Bots(bots.map(bot => {
       bot.host = this;
       bot.reply = () => {};
       bot.enable(true);
       return bot;
-    });
+    }));
   }
 
   onMessage({ from, content }) {
     try {
-      this.#bots.forEach(bot => bot.reply = this.reply.bind(this, bot));
+      this.#bots.setReply(this.reply.bind(this));
 
       // If it's a message from a bot, then ignore it.
       if (from.startsWith(Bot.PREFIX))
         return;
 
       // First let's see if it's a general message, or if it's directed at a specific bot.
-      const enabledBots = this.#bots.filter(bot => bot.enabled);
+      const mentionedBot = this.#bots.findMentionedBot(content);
 
-      const bot = enabledBots.find(bot => content.startsWith(bot.name));
-
-      if (bot) {
+      if (mentionedBot) {
         // Then it's directed at this specific bot and this one alone.
-        content = content.substring(bot.name.length + Bot.PREFIX.length);
-        logger.info(`Calling bot ${bot.name} with directed message ${content}`);
+        content = content.substring(mentionedBot.name.length + Bot.PREFIX.length);
+        logger.info(`Calling bot ${mentionedBot.name} with directed message ${content}`);
 
-        bot.onDirectMessage({ from, content });
+        mentionedBot.onDirectMessage({ from, content });
       } else {
         logger.info(`Calling all enabled bots with general message ${content}`);
 
-        enabledBots.forEach(bot => {
+        this.#bots.getEnabledBots().forEach(bot => {
           bot.onPublicMessage({ from, content });
         });
       }
     } catch (e) {
       logger.error(`Error handling message ${e.stack}, continuing.`);
     }
-  }
-
-  getBotMetadata() {
-    return this.#bots.map(bot => ({
-      name: bot.name,
-      description: bot.description,
-      enabled: bot.enabled,
-      enableable: bot.enableable,
-      tests: bot.getTests(),
-    }));
   }
 
   reply(bot, content, to) {
@@ -71,20 +60,11 @@ export default class Host {
     this.#channels.listen();
   }
 
+  getBotMetadata() {
+    return this.#bots.describeBots();
+  }
+
   enableBot(botName, on) {
-    const bot = this.#bots.find(bot => bot.name === botName);
-
-    if (!bot)
-      return null;
-
-    // If on is not passed, then flip the state.
-    // Else, set to the defined state in on.
-    if (on === undefined) {
-      bot.enable(!bot.enabled);
-    } else {
-      bot.enable(on);
-    }
-
-    return bot.enabled;
+    return this.#bots.enableBot(botName, on);
   }
 }
