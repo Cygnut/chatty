eaa1d51a391a6ce57bcb088515c6b4c0c9042c6a
lee <lee@feeditback.co.uk>
2022-03-08 12:49:58 +0000

    sigh

--- bots/Goodreads.js
+++ bots/Goodreads.js
@@ -10,13 +10,13 @@ export default class Goodreads extends Bot {
   #rootUrl = 'https://www.goodreads.com/';
   #apiKey;
 
-  constructor(goodreadsApiKey) {
+  constructor({ apiKey }) {
     super({
       name: 'goodreads',
       description: "Finds the top related book for a given search term."
     });
 
-    this.#apiKey = goodreadsApiKey;
+    this.#apiKey = apiKey;
   }
 
   getTests() {
@@ -35,7 +35,7 @@ export default class Goodreads extends Bot {
       // Get the search result for this search term, specifically, the top related book.
       const url = `${this.#rootUrl}search/index.xml?key=${this.#apiKey}&q=${content}`;
       const response = await fetch(url);
-      const body = await response.json();
+      const body = await response.text();
       const parser = new xml2js.Parser();
       const result = await parser.parseStringPromise(body);
       const title = result.GoodreadsResponse.search[0].results[0].work[0].best_book[0].title[0];
