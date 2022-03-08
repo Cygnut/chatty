3988ce84020f1a5991832da14ad933f084dbcaff
lee <lee@feeditback.co.uk>
2022-03-08 11:14:57 +0000

    note

--- AppPath.js
+++ AppPath.js
@@ -5,4 +5,5 @@ import { fileURLToPath } from 'url';
 // we're advised to use this method.
 //   This is also more reliable than using process.cwd(), as it will return the same value even if
 // your working directory differs from the actual directory containing the path.
+//   NOTE: This file must be placed in the same directory as your app.js file.
 export default dirname(fileURLToPath(import.meta.url));
