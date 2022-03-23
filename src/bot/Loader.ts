45cc8b9631d04182767b972ba67b61bc042499da
Cygnut <ligand@hotmail.co.uk>
2022-03-23 22:19:55 +0000

    more ts fixes, and also this is the way this should have been done

--- src/bot/Loader.ts
+++ src/bot/Loader.ts
@@ -8,39 +8,6 @@ import appPath from '../AppPath';
 // Only pick up files which have at least one character before Bot.ts.
 const botRegex = /.+.ts/
 
-type Settings = Record<string, string>;
-
-const combineSettings = (common: Settings, specific: Settings) => {
-  const combined = {};
-
-  // Copy common into combined to start with.
-  for (const prop in common) {
-    // Iterate over all of common's own properties.
-    if (common.hasOwnProperty(prop)) {
-      combined[prop] = common[prop];
-    }
-  }
-
-  // If specific has nothing, then we're done.
-  if (specific === null || specific === undefined)
-    return combined;
-
-  for (const property in specific) {
-    if (specific.hasOwnProperty(property)) {
-      // Iterate over all of specific's own properties.
-
-      // If there's a clash between the properties that common and specific has, use common.
-      if (combined.hasOwnProperty(property))
-        continue;
-
-      // Otherwise, add the properties from specific
-      combined[property] = specific[property];
-    }
-  }
-
-  return combined;
-};
-
 const tryCreateBot = async (filepath: string) => {
   // Load the config file
   let importee = null;
@@ -58,13 +25,11 @@ const tryCreateBot = async (filepath: string) => {
 
   // Get the bot specific settings for this bot.
   const botConfig = config.bots.find(bot => bot.name === importedClass.name);
-
   const botSettings = botConfig ? botConfig.settings : {};
-  const settings = combineSettings({}, botSettings);
 
   // Instantiate the bot with those settings.
   try {
-    return new importedClass(settings);
+    return new importedClass(botSettings);
   } catch (e) {
     throw new Error(`Failed to load bot ${importedClass.name}. ${e} ${e.stack}`);
   }
